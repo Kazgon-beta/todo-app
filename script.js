84c772a37a6e9819d2d6aa8006f153df554bc42d
kazgon-beta <tyutoro96@icloud.com>
2025-11-03 20:29:42 +0900

    タスクを追加してボタンでリストに表示する機能を実装

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("TODOアプリ準備中");
+const input = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
+
+addButton.addEventListener("click", () => {
+  const text = input.value.trim(); // 余分な空白カット
+  if (text === "") return; // から文字は追加しない
+
+  // <li>～</li> を作る
+  const li = document.createElement("li");
+  li.textContent = text;
+
+  // リストに入れる
+  taskList.appendChild(li);
+
+  // 入力欄をリセット
+  input.value = "";
+});
